220deacab37dbc769b4a0bae75792aa82deb16e9
chenmi1997 <46470453+chenmi1997@users.noreply.github.com>
2019-02-22 16:41:42 -0500

    Update robo_advisor.py

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,42 +1,93 @@
-from dotenv import load_dotenv
+# goal: print the latest closing price
+
 import json
 import os
 import requests
+from dotenv import load_dotenv
 
-load_dotenv() # loads environment variables set in a ".env" file, including the value of the ALPHAVANTAGE_API_KEY variable
+# load_dotenv() #> loads contents of the .env file into the script's environment
 
-# see: https://www.alphavantage.co/support/#api-key
-api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
-#print("API KEY: " + api_key)
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
 
-symbol = "NFLX" # TODO: capture user input, like... input("Please specify a stock symbol: ")
+# API_KEY = os.environ.get("MY_API_KEY")
+#print(API_KEY)
 
-# see: https://www.alphavantage.co/documentation/#daily (or a different endpoint, as desired)
-# TODO: assemble the request url to get daily data for the given stock symbol...
+# "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&apikey=demo"
+#request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&apikey=" + API_KEY
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
+print(request_url)
 
-# TODO: use the "requests" package to issue a "GET" request to the specified url, and store the JSON response in a variable...
+response = requests.get(request_url)
 
-# TODO: further parse the JSON response...
+print("RESPONSE STATUS: " + str(response.status_code))
+#print("RESPONSE TEXT: " + response.text)3. L
 
-# TODO: traverse the nested response data structure to find the latest closing price and other values of interest...
-latest_price_usd = "$100,000.00"
+parsed_response = json.loads(response.text)
+
+tsd = parsed_response["Time Series (Daily)"] #> 'dict'
+
+dates = list(tsd.keys())
+
+latest_day = "2019-02-20" # TODO: make dynamic
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+latest_close = tsd[latest_day]["4. close"]
+
+# max of high prices
+high_prices = []
+low_prices = []
+
+for date in dates:
+    high_price = tsd[date]["2. high"]
+    high_prices.append(float(high_price))
+    low_price = tsd[date]["3. low"]
+    low_prices.append(float(low_price))
+
+recent_high = max(high_prices)
+recent_low = min(low_prices)
+
+
+
+print(parsed_response["Time Series (Daily)"]["2019-02-19"]["4. close"])
+# > '1627.5800'
+
 
 #
 # INFO OUTPUTS
 #
 
-# TODO: write response data to a CSV file
+print("--------------------------------")
+print("SELECTED SYMBOL: MSFT")
+print("--------------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("--------------------------------")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"THE LATEST CLOSE: {to_usd(float(latest_close))}")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
+print("--------------------------------")
+print("RECOMMENDATION: BUY!")
+print("BECAUSE: TODO")
+print("--------------------------------")
+print("HAPPY INVESTING")
+print("--------------------------------")
 
-# TODO: further revise the example outputs below to reflect real information
-print("-----------------")
-print(f"STOCK SYMBOL: {symbol}")
-print("RUN AT: 11:52pm on June 5th, 2018")
-print("-----------------")
-print("LATEST DAY OF AVAILABLE DATA: June 4th, 2018")
-print(f"LATEST DAILY CLOSING PRICE: {latest_price_usd}")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
-print("-----------------")
-print("RECOMMENDATION: Buy!")
-print("RECOMMENDATION REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
-print("-----------------")
+#
+# BUT THE LATEST DAY WON'T ALWAYS BE "2019-02-19"
+# ... SO HERE'S A WAY TO GET THE LATEST DAY, WHATEVER IT IS
+#
+
+#
+# What keys or attributes does this dictionary have?
+# ... see: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/datatypes/dictionaries.md
+
+
+#
+# print(parsed_response["Time Series (Daily)"][latest_day]["4. close"])
+# > '1627.5800'
+#
+# print(tsd[latest_day]["4. close"])
+# > '1627.5800'
+
