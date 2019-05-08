f6fc855fca1df281281fa81cb792a2150f9c6d79
chenmi1997 <46470453+chenmi1997@users.noreply.github.com>
2019-05-08 14:30:22 -0400

    Update robo_test.py

--- app/robo_test.py
+++ app/robo_test.py
@@ -6,6 +6,6 @@ def test_usd():
     result = to_usd(10)
     result2 = to_usd(57.3333)
     result3 = to_usd(.011111111)
-    assert result == "$ 10.00"
-    assert result2 == "$ 57.33"
-    assert result3 == "$ 0.01"
+    assert result == "$10.00"
+    assert result2 == "$57.33"
+    assert result3 == "$0.01"
